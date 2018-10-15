8749380400f51b8f9206df98798dd1bf5cc64206
94Me <me94me@qq.com>
2018-10-15 19:21:16 +0800

    dagger2源码

---
+++ PreferredLibrary/Dagger2/source_example_dagger2/app/src/main/java/com/me94me/source_example_dagger2/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.me94me.source_example_dagger2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
